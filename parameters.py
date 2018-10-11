eda2605ef85b25cee1ea480fe84f9de7ab09a5cd
Rosalind Wang <rosalind@rosalindwang.net>
2018-10-12 10:32:36 +1100

    Add verbose option for training and prediction

--- parameters.py
+++ parameters.py
@@ -2,6 +2,7 @@ training_options = {
     'epochs': 50,
     'batch_size': 128,
     'validation_split': 0.2,
+﻿    'verbose_training': 2, # 0 = silent, 1 = progress bar, 2 = one line per epoch 
     'adam_optimizer_options': {'lr': 0.001, 'beta_1': 0.9, 'beta_2': 0.999},
     'train_with_gpu': True,
     'random_seed_type': 1,  # Type 1 for repeatability within repetitions, type 2 for random number using clock time, type 3 for previously used number
@@ -54,5 +55,7 @@ prediction_options['predictions_save_name'] = '{}/{}_{}_Prediction.csv'.format(p
 batch_prediction_options = {
     'save_names': ["ModelTests-On{}.csv".format(x.split('-')[4]) for x in training_options['datasets']],
     'model_repeats': range(1,2),
-    'model_variants': [20, 21, 26] # range(1, 28)
+﻿    'model_names': ['D'], # ['A', 'B', 'C', 'D', 'E', 'F', 'G'],  
+    'model_variants': [20, 21, 26], # range(1, 28),
+﻿    'verbose_prediction' : 0     
 }
